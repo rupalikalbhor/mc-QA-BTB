12788631cc6c2086493ec766ed23df35becae66a
Rupali <r.kalbhor@modcloth.com>
2012-08-02 14:05:08 -0700

    Added screenshot code

--- Gemfile
+++ Gemfile
@@ -1,13 +1,9 @@
 source "http://rubygems.org"
 
-#gem 'ci_reporter'
 gem 'capybara', '1.1.2'
 gem 'capybara-webkit', '0.11.0'
 gem 'rspec', '2.10.0'
 gem 'yajl-ruby', '1.1.0'
-#gem 'mysql','2.8.1'
-#gem 'net-ssh-gateway', '1.1.0'
-#gem 'mysql2', '0.3.11'
 gem 'selenium-webdriver'
 gem 'capybara-screenshot'
 gem 'postgres-pr'
--- spec/Test/test_spec.rb
+++ spec/Test/test_spec.rb
@@ -4,10 +4,10 @@ require 'database_support/database_helper'
 describe 'Testing' do
   it 'Visit to homepage.', :type => :request do
 
-    #puts 'enter iinto test heyyyy'
-    #visit 'http://www.modcloth.com'
-    #puts 'site exit !!!!!!!'
-    #page.should have_content ("Sign out")
-    connection
+    puts 'enter iinto test heyyyy'
+    visit 'http://www.modcloth.com'
+    puts 'site exit !!!!!!!'
+    page.should have_content ("Sign out")
+    #connection
   end
 end
