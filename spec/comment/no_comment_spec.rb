f7471e1bc8bf14cbb7f7d2ae8b94e476003fcec3
Rupali <r.kalbhor@modcloth.com>
2012-08-02 17:14:48 -0700

    added comment folder

---
+++ spec/comment/no_comment_spec.rb
@@ -0,0 +1,11 @@
+describe 'Sample with no comments' do
+  before(:each) do
+    go_to_BTB_page
+  end
+
+  it '1. Verify when user sign out from wishlist management page, user navigates to Sign or Join page.' do
+
+  end
+
+
+end
